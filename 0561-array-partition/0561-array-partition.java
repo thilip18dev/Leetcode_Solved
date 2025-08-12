3e6a1f5b7095979de47ac8142bcda01c1682d41b
Thiliban L.N <thilipl127@gmail.com>
2025-08-12 15:18:26 +0530

    Time: 13 ms (28.22%), Space: 46.8 MB (48.53%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int sum=0;
+        for(int i=0;i<n;i=i+2){
+            sum+=nums[i];
+        }return sum;
+    }
+}
