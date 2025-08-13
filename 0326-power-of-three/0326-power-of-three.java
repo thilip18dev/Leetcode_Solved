55aa37ba6978d2c73888085b9e517c6f8e07a9fe
Thiliban L.N <thilipl127@gmail.com>
2025-08-13 10:44:15 +0530

    Time: 14 ms (6.99%), Space: 44.8 MB (5.71%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+     for(int i=0;i<20;i++){
+       if( Math.pow(3,i)==n){
+        return true;
+       }
+     }return false;
+    }
+}
