786d4aa620bbde5b490ce01ccea7ffaafe8e1a35
Thiliban L.N <thilipl127@gmail.com>
2025-08-29 13:31:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        int a=0;
+        int b=1;
+        if(n<=1) return 1;
+        for(int i=2;i<=n;i++){
+          int sum=a+b;
+          a=b;
+          b=sum;
+        }return b;
+    }
+}
