5cd39417bffacceb639572c700ba3b0e94b4f1a2
Thiliban L.N <thilipl127@gmail.com>
2025-09-15 10:20:48 +0530

    Time: 96 ms (60.73%), Space: 49.3 MB (47.28%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countPrimes(int n) {
+      boolean arr[]=new boolean[n];
+      if(arr.length<=2) return 0;
+      Arrays.fill(arr,true);
+      arr[0]=false;
+      arr[1]=false;
+      for(int i=2;i*i<n;i++){
+        if(arr[i]){
+            for(int j=i*i;j<n;j+=i){
+                arr[j]=false;
+            }
+        }
+      }  int count=0;
+      for(int i=2;i<n;i++){
+        if(arr[i]==true){
+            count++;
+        }
+      }return count;
+    }
+}
